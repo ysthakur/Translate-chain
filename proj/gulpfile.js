03aa79d8df40f4cba42b3edab39f47f44db6d76e
ysthakur <2004yst@gmail.com>
2021-02-08 17:00:33 -0500

    Got it working

--- proj/gulpfile.js
+++ proj/gulpfile.js
@@ -1,35 +1,49 @@
 var gulp = require("gulp");
 var browserify = require("browserify");
 var source = require("vinyl-source-stream");
-var watchify = require("watchify");
 var tsify = require("tsify");
-var fancy_log = require("fancy-log");
+// var uglify = require("gulp-uglify");
+var terser = require("gulp-terser");
+var sourcemaps = require("gulp-sourcemaps");
+var buffer = require("vinyl-buffer");
 var paths = {
   pages: ["src/*.html"],
 };
+var ts = require("gulp-typescript");
+var tsProject = ts.createProject("tsconfig.json");
+const babel = require('gulp-babel');
 
-var watchedBrowserify = watchify(
-  browserify({
-    basedir: ".",
-    debug: true,
-    entries: ["src/main.ts"],
-    cache: {},
-    packageCache: {},
-  }).plugin(tsify)
-);
-
-gulp.task("copy-html", function () {
-  return gulp.src(paths.pages).pipe(gulp.dest("dist"));
+gulp.task("default", function () {
+  return tsProject
+    .src()
+    .pipe(tsProject())
+    .js
+    .pipe(
+      babel({
+        presets: [['@babel/preset-env']]
+      })
+    ).pipe(gulp.dest("dist"));
 });
 
-function bundle() {
-  return watchedBrowserify
-    .bundle()
-    .on("error", fancy_log)
-    .pipe(source("bundle.js"))
-    .pipe(gulp.dest("dist"));
-}
+gulp.task("copy-html", () => gulp.src(paths.pages).pipe(gulp.dest("dist")));
 
-gulp.task("default", gulp.series(gulp.parallel("copy-html"), bundle));
-watchedBrowserify.on("update", bundle);
-watchedBrowserify.on("log", fancy_log);
+// gulp.task(
+//   "default",
+//   gulp.series(gulp.parallel("copy-html"), function () {
+//     return browserify({
+//       basedir: ".",
+//       debug: true,
+//       entries: ["src/main.ts"],
+//       cache: {},
+//       packageCache: {},
+//     })
+//       .plugin(tsify)
+//       .bundle()
+//       .pipe(source("bundle.js"))
+//       .pipe(buffer())
+//       .pipe(sourcemaps.init({ loadMaps: true }))
+//       // .pipe(terser())
+//       .pipe(sourcemaps.write("./"))
+//       .pipe(gulp.dest("dist"));
+//   })
+// );
--- proj/src/main.ts
+++ proj/src/main.ts
@@ -1,54 +1,84 @@
-import translate = require('@vitalets/google-translate-api');
+import * as googleTranslateApi from '@vitalets/google-translate-api';
+import * as path from 'path';
+const bodyParser = require('body-parser');
+const express = require('express');
+const tr = require('@vitalets/google-translate-api');
+const app = express()
+const port = 3000
 
-type ResType = {
-  text: string,
-  from: {
-    language: {
-      iso: string
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, '../views'))
+
+// app.get('/', (req: any, res: any) => {
+//   res.send('Hello World!')
+// })
+
+const allLanguageNames = <string[]> Object.values(googleTranslateApi.languages).filter(l => typeof l === "string");
+
+let startLang = 'auto',
+  endLang = 'en',
+  middleLangs: string[] = [],
+  middleLangNames: string[] = [],
+  inputText = "",
+  outputText = "";
+
+function rerender(res: any) {
+  res.render('pages/index.ejs', {
+    startLang: tr.languages[startLang],
+    endLang: tr.languages[endLang],
+    middleLangs: JSON.stringify(middleLangNames),
+    inputText: inputText,
+    outputText: outputText,
+    allLangs: JSON.stringify(allLanguageNames)
+  })
+}
+
+function translate(text: string, from: string, to: string): Promise<string> {
+  return tr(text, { from: from, to: to }).then((res: googleTranslateApi.ITranslateResponse) => {
+      // console.log(res);
+      return res.text
+    })
+}
+
+// index page
+app.get('/', (req: any, res: any) => {
+  rerender(res);
+});
+
+app.post('/', (req: any, res: any) => {
+  // res.sendStatus(200);
+  // console.log('Got body:', req.body);
+  if (req.body['Action kind'] === 'Input text') {
+    inputText = req.body['Input text'];
+    let from = startLang;
+    let promise = Promise.resolve(inputText);
+    for (const to of middleLangs) {
+      const f2 = from;
+      const t2 = to;
+      // console.log(`from=${from}, to=${to}`);
+      promise = promise.then(text => translate(text, f2, t2));
+      from = to;
     }
+    promise.then(text => translate(text, from, endLang))
+      .then(finalText => {
+        outputText = finalText;
+        rerender(res);
+      }).catch(e => console.error(e));
+  } else {
+    startLang = tr.languages.getCode(req.body['Start language']);
+    endLang = tr.languages.getCode(req.body['End language']);
+    const added = req.body['Added language'];
+    if (added) {
+      middleLangs.push(tr.languages.getCode(added));
+      middleLangNames.push(added);
+    }
+    rerender(res);
   }
-}
+});
 
-interface StringIndices {
-  [index: string]: any
-}
-
-const languages = <StringIndices> translate.languages;
-
-let startLang: string | null = null;
-let endLang: string | null = null;
-let middleLangs: string[] = [];
-
-const startLanguageBox = <HTMLInputElement> document.getElementById('Start language input');
-const endLanguageBox = <HTMLInputElement> document.getElementById('End language input');
-const addLanguageBox = <HTMLInputElement> document.getElementById('Add-language-input');
-
-startLanguageBox.onkeydown = (event: KeyboardEvent) => {
-  if (event.key == 'Enter') {
-    startLang = startLanguageBox.value;
-    console.log(startLang);
-  }
-};
-
-endLanguageBox.onkeydown = (event: KeyboardEvent) => {
-  if (event.key == 'Enter') {
-    endLang = endLanguageBox.value;
-    console.log(endLang);
-  }
-};
-
-addLanguageBox.onkeydown = (event: KeyboardEvent) => {
-  if (event.key == 'Enter') {
-    middleLangs.push(addLanguageBox.value);
-    console.log(middleLangs);
-  }
-};
-
-translate('Ik spreek Engels', {to: 'en'}).then(res => {
-    console.log(res.text);
-    //=> I speak English
-    console.log(res.from.language.iso);
-    //=> nl
-}).catch(err => {
-    console.error(err);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 });
