b8b1023b5fe364a5cb6de57319c736ce372f6dad
ysthakur <2004yst@gmail.com>
2021-02-18 18:34:02 -0500

    Finishing touches

--- proj/src/main.ts
+++ proj/src/main.ts
@@ -14,15 +14,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.set("view engine", "ejs")
 app.set('views', path.join(__dirname, '../views'))
 
-const allLanguageNames = JSON.stringify(Object.values(tr.languages).filter(l => typeof l === "string"))
 const allLangsButAuto = JSON.stringify(Object.values(tr.languages).filter(l => typeof l === "string" && l != "Automatic"))
 
 function trans(text: string, from: string, to: string) {
   // return translate(text, { from: from, to: to }).then((res: tr.ITranslateResponse) => res.text)
   return translate(text, { from: from, to: to }).then((res: tr.ITranslateResponse) => res.text)
 }
-console.log(JSON.stringify([]));
-console.log(JSON.stringify(['foo', 'bar']));
+
 // index page
 app.get('/', (req: any, res: any) =>
   res.render('pages/index.ejs', {
@@ -31,22 +29,17 @@ app.get('/', (req: any, res: any) =>
     middleLangs: '[]',
     inputText: '',
     outputText: '',
-    allLangs: allLanguageNames,
     allButAuto: allLangsButAuto
   })
 )
 
 app.post('/', (req: any, res: any) => {
-  console.log(req.body);
-  
   let startLang = req.body['Start language'],
     endLang = req.body['End language'],
     middleLangs: string[] = eval(req.body['Middle languages']),
     inputText = req.body['Input text'],
     outputText = req.body['Output text'],
     actionKind = req.body['Action kind'];
-
-  console.log(`MiddleLangs=${middleLangs} -from- ${req.body['Middle languages']}`)
 
   let rerender = () =>
     res.render('pages/index.ejs', {
@@ -55,7 +48,6 @@ app.post('/', (req: any, res: any) => {
       middleLangs: JSON.stringify(middleLangs),
       inputText: inputText,
       outputText: outputText,
-      allLangs: allLanguageNames,
       allButAuto: allLangsButAuto
     })
 
@@ -74,13 +66,6 @@ app.post('/', (req: any, res: any) => {
         rerender()
       }).catch(e => console.error(e))
   } else {
-    console.log({
-      startLang: startLang,
-      endLang: endLang,
-      middleLangs: JSON.stringify(middleLangs),
-      inputText: inputText,
-      outputText: outputText
-    })
     rerender()
   }
 })
